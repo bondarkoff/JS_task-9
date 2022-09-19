4b946e02b70cd5b8e7739554404fc593e8a3adba
bondarkoff <yehor.bondarkoff@gmail.com>
2022-09-19 13:04:03 +0300

    js_task-9

---
+++ script.js
@@ -0,0 +1,9 @@
+// console.log(document.head);
+// console.log(document.documentElement);
+// console.log(document.body.childNodes);
+// console.log(document.body.firstChild);
+// console.log(document.body.lastChild);
+
+console.log(document.querySelector('#current').parentNode.parentNode);
+
+console.log(document.querySelector('[data-current="3"]').nextSibling);
