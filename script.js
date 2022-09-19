06bf8f36b678dc68edd387c8978f6c303fc145e7
bondarkoff <yehor.bondarkoff@gmail.com>
2022-09-19 13:04:20 +0300

    js_task-9

--- script.js
+++ script.js
@@ -1,9 +1,74 @@
-// console.log(document.head);
-// console.log(document.documentElement);
-// console.log(document.body.childNodes);
-// console.log(document.body.firstChild);
-// console.log(document.body.lastChild);
+let students = {
+    js: [{
+        name: 'John',
+        progress: 100
+    }, {
+        name: 'Ivan',
+        progress: 60
+    }],
+    html: {
+        basic: [{
+            name: 'Peter',
+            progress: 20
+        }, {
+            name: 'Ann',
+            progress: 18
+        }],
+        pro: [{
+            name: 'Sam',
+            progress: 10
+        }]
+    }
+};
 
-console.log(document.querySelector('#current').parentNode.parentNode);
+// Методом цикла (итерация)
+function getTotalProgressByIteration(data) {
+    let total = 0;
+    let students = 0;
 
-console.log(document.querySelector('[data-current="3"]').nextSibling);
+    for (let course of Object.values(data)) {
+        if (Array.isArray(course)) {
+            students += course.length;
+
+            for (let i = 0; i < course.length; i++) {
+                total += course[i].progress;
+            }
+        } else {
+            for (let subCourse of Object.values(course)) {
+                students += subCourse.length;
+
+                for (let i = 0; i < subCourse.length; i++) {
+                    total += subCourse[i].progress;
+                }
+            }
+        }
+    }
+
+    return total / students;
+}
+console.log(getTotalProgressByIteration(students));
+
+// Методом рекурсии
+function getTotalProgressByRecursion(data) {
+    if (Array.isArray(data)) {
+        let total = 0;
+
+        for (let i = 0; i < data.length; i++) {
+            total += data[i].progress;
+        }
+
+        return [total, data.length];
+    } else {
+        let total = [0, 0];
+
+        for (let subData of Object.values(data)) {
+            const subDataArr = getTotalProgressByRecursion(subData);
+            total[0] += subDataArr[0];
+            total[1] += subDataArr[1];
+        }
+        return total;
+    }
+}
+
+const result = getTotalProgressByRecursion(students);
+console.log(result[0]/result[1]);
